7945bb4bb7e62fab614aaca0be4c296886199073
EgorLozh <lozhkinyegor@yandex.ru>
2024-06-15 22:19:10 +0400

    google_trend_parser

---
+++ Scripts/parsers/Get_google_trends.py
@@ -0,0 +1,27 @@
+import requests
+import json
+def get_google_trends():
+    headers = {
+        'authority': 'trends.google.com',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'ru,en;q=0.9',
+        'dnt': '1',
+        'referer': 'https://trends.google.com/trends/trendingsearches/daily?geo=RU&hl=ru',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    }
+
+    params = [
+        ('hl', 'ru'),
+        ('tz', '-240'),
+        ('geo', 'RU'),
+        ('hl', 'ru')
+    ]
+
+    response = requests.get('https://trends.google.com/trends/api/dailytrends', params=params,
+                            headers=headers).text.replace(")]}',", "", 1)
+    json_response = json.loads(response)
+    main_info = json_response['default']['trendingSearchesDays'][0]['trendingSearches']
+    print(main_info)
+
+if __name__=="__main__":
+    get_google_trends()
